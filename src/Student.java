b28c6ccea28bcef8e58ad14819709015886cacfd
rmin-mohd <152813709+rmin-mohd@users.noreply.github.com>
2024-10-26 14:37:03 +0330

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,50 +1,70 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Student {
 
     private int age;
     private String name;
-    private String Id;
+    private String id;
     private double grade;
 
-    // Constructor
-    public Student(int age, String name, String Id) {
+    public Student(int age, String name, String id) {
         this.age = age;
         this.name = name;
-        this.Id = Id;
-        this.grade = 0; // default grade
+        this.id = id;
+        this.grade = 0;
     }
 
-    // Method to print student info
     public void printStudentInfo() {
-        System.out.println("Name: " + name + ", Age: " + age + ", Id: " + Id + ", Grade: " + grade);
+        System.out.println(name + " - ID: " + id + " - grade: " + grade);
     }
 
-    // Method to set grade
     public void setGrade(double grade) {
         this.grade = grade;
     }
 
-    // Method to get grade
-    public double getGrade() {
-        return grade;
-    }
-
-    // Main method
     public static void main(String[] args) {
-        // Corrected object creation with int for age
-        Student std1 = new Student(18, "Mohd", "401314");
+        Student std1 = new Student(15, "Raza", "410314");
+        std1.setGrade(20.0);
         std1.printStudentInfo();
-
-        Student std2 = new Student(20, "Raza", "401315");
+        
+        Student std2 = new Student(20, "Mohd", "410315");
+        std2.setGrade(19.0);
+        std1.printStudentInfo();
         std2.printStudentInfo();
 
-        // Set grade values (note the use of double)
-        std1.setGrade(19.0);
-        std2.setGrade(21.0);
+        Lab lab = new Lab("Mr.Smith", "Monday", 30);
+        lab.addStudent(std1);
+        lab.addStudent(std2);
 
-        // Print grades to confirm
-        System.out.println("Updated Grade for " + std1.name + ": " + std1.getGrade());
-        System.out.println("Updated Grade for " + std2.name + ": " + std2.getGrade());
+        lab.printLabInfo();
+    }
+}
+
+class Lab {
+
+    private String teacherName;
+    private String dayOfWeek;
+    private int maxSize;
+    private ArrayList<Student> students;
+
+    public Lab(String teacherName, String dayOfWeek, int maxSize) {
+        this.teacherName = teacherName;
+        this.dayOfWeek = dayOfWeek;
+        this.maxSize = maxSize;
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        if (students.size() < maxSize) {
+            students.add(student);
+        } else {
+            System.out.println("Lab is full. Cannot add more students.");
+        }
+    }
+
+    public void printLabInfo() {
+        System.out.println("Teacher: " + teacherName);
+        System.out.println("Day of Week: " + dayOfWeek);
+        System.out.println("Max Size: " + maxSize);
     }
 }
